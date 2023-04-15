848cb3002ebc29480bcf95fd869105eb078e2d4d
Ednardo Rubens <ednardorubens@hotmail.com>
2023-04-15 16:24:38 -0300

    Project Base

---
+++ src/main/java/br/com/sccon/rh/ScconRhApplication.java
@@ -0,0 +1,13 @@
+package br.com.sccon.rh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScconRhApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScconRhApplication.class, args);
+    }
+
+}
