63e2be44b2feb04c4463804afba1fc706318967f
Ednardo Rubens <ednardorubens@hotmail.com>
2023-04-18 12:01:18 -0300

    Update validation

--- src/main/java/br/com/sccon/rh/domain/Person.java
+++ src/main/java/br/com/sccon/rh/domain/Person.java
@@ -4,6 +4,9 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
 import java.time.LocalDate;
 
 @Data
@@ -12,8 +15,14 @@ import java.time.LocalDate;
 public class Person {
 
     private Integer id;
+
+    @NotBlank
     private String name;
+
+    @NotNull
     private LocalDate dateOfBirth;
+
+    @NotNull
     private LocalDate dateOfAdmission;
 
     public Person(String name, LocalDate dateOfBirth, LocalDate dateOfAdmission) {
